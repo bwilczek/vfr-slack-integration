69b8b5033ece162f4c76f6ff5651aeef62a6cc6f
Bartek Wilczek <bwilczek@gmail.com>
2016-12-14 21:57:18 +0100

    Progress in support for NOTAMs

--- server.rb
+++ server.rb
@@ -1,18 +1,32 @@
 require 'sinatra'
 require 'json'
+require 'vfr-utils'
 
 set :bind, '0.0.0.0'
 
 get '/' do
-	'Try with POST'
+	handle_request
+	# 'Try with POST'
 end
 
 post '/' do
-	content_type 'application/json'
+	handle_request
+end
+
+def handle_request
 	# TODO: validate ENV['SLACK_TOKEN'] == params[:token]
 	# TODO: validate params[:text]
+
+	content_type 'application/json'
+
+	operation = params[:text].split.first
+	operation_params = params[:text].split[1..-1]
+
+	# return "|#{operation}| #{operation_params.to_s}"
+	view = erb :notam_multiple_airports, locals: { data: VfrUtils::NOTAM.get(operation_params) }
+
 	ret = {
-		"text": "So, does it work?\nText: _#{params[:text]}_",
+		"text": view,
 		"username": "VFR bot",
 		"mrkdwn": true
 	}
