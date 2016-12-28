55ea0e1ca571bdbf809999ca00f081767f4cdd79
Bartek Wilczek <bwilczek@gmail.com>
2016-12-28 23:48:28 +0100

    Improved error logging

--- worker.rb
+++ worker.rb
@@ -1,15 +1,16 @@
 require 'faraday'
 require 'vfr_utils'
+require 'logger'
 require_relative './lib/rabbit_helper'
 require_relative './lib/markdown_formatter'
+
+logger = Logger.new('/tmp/vfr_utils.log')
+logger.level = Logger::DEBUG # Logger::FATAL
 
 RabbitHelper.init
 
 begin
   RabbitHelper.process_message do |data|
-
-    debug = true
-
     result = case data['operation']
       when 'notam' then MarkdownFormatter.notam(VfrUtils::NOTAM.get(data['operation_params']))
       when 'metar'
@@ -22,21 +23,16 @@ begin
         next
       end
 
-    if debug
-      $stdout.puts " > Posting #{result.length} bytes to Slack"
-    end
+    logger.debug " > Posting #{result.length} bytes to Slack" if debug
 
     response = Faraday.post do |req|
       req.url data['response_url']
       req.headers['Content-Type'] = 'application/json'
       req.body = JSON.generate(text: result, mrkdwn: true)
     end
-    # unless response.status == 200
-    #   $stderr.puts "POST to Slack failed. Code: #{response.status}, body: #{response.body}"
-    # end
-
-    if debug
-      $stdout.puts " < Result code: #{response.status}, body: #{response.body}"
+    logger.debug " < Result code: #{response.status}, body: #{response.body}"
+    unless response.status == 200
+      logger.error "POST to Slack failed. Code: #{response.status}, body: #{response.body}"
     end
 
   end
